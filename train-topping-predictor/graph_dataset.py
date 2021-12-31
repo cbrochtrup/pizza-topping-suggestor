0cc919f823e7ff51cfdc84ddaed5dd46d240d86b
Colin Brochtrup <cbrochtrup@greenkeytech.com>
2021-12-31 12:21:49 -0600

    Add code to predict edges for a smaller user defined graph. Right now it only predicts the inputs... WG'

--- train-topping-predictor/graph_dataset.py
+++ train-topping-predictor/graph_dataset.py
@@ -5,11 +5,16 @@ import torch
 import numpy as np
 
 class PizzaToppingDataset(DGLDataset):
-    def __init__(self):
+    edge_data_ = None
+    def __init__(self, edge_data=None):
+        self.edge_data_ = edge_data
         super().__init__(name='pizza_toppings')
 
     def process(self):
         edges_data = pd.read_csv('edges.csv')
+        if self.edge_data_ is not None:
+          print('UUUUUUUUUUUUUUUUUUUU')
+          edges_data = self.edge_data_
         edges_src = torch.from_numpy(edges_data['Src'].to_numpy())
         edges_dst = torch.from_numpy(edges_data['Dst'].to_numpy())
         edges_that_exist = edges_data['Src'].unique()
@@ -19,7 +24,7 @@ class PizzaToppingDataset(DGLDataset):
             )
             # gedges_data['Weight1'].to_numpy()
         ).float().T
-        print(f'edge_features are shape {edge_features.shape}')
+        print(edge_features.shape)
 
         nodes_data = pd.read_csv('nodes.csv')
         # nodes_data = nodes_data[nodes_data.isin(edges_that_exist)]
@@ -33,21 +38,6 @@ class PizzaToppingDataset(DGLDataset):
         self.graph.ndata['feat'] = node_features
         # self.graph.ndata['label'] = torch.from_numpy(node_cat).long()
         self.graph.edata['weight'] = edge_features
-
-        # If your dataset is a node classification dataset, you will need to assign
-        # masks indicating whether a node belongs to training, validation, and test set.
-        # n_nodes = nodes_data.shape[0]
-        # n_train = int(n_nodes * 0.6)
-        # n_val = int(n_nodes * 0.2)
-        # train_mask = torch.zeros(n_nodes, dtype=torch.bool)
-        # val_mask = torch.zeros(n_nodes, dtype=torch.bool)
-        # test_mask = torch.zeros(n_nodes, dtype=torch.bool)
-        # train_mask[:n_train] = True
-        # val_mask[n_train:n_train + n_val] = True
-        # test_mask[n_train + n_val:] = True
-        # self.graph.ndata['train_mask'] = train_mask
-        # self.graph.ndata['val_mask'] = val_mask
-        # self.graph.ndata['test_mask'] = test_mask
 
     def __getitem__(self, i):
         return self.graph
--- train-topping-predictor/predict.py
+++ train-topping-predictor/predict.py
@@ -1,18 +1,38 @@
 import torch
+import itertools
+import pandas as pd
 import json
-from link_predict import model, pred, g
-
-# TODO make a graph from very few toppings and get predicted links
-# TODO? Figure out how to save and load dgl models
+import sys
+from link_predict import model, pred
+from find_toppings import build_edge
+from graph_dataset import PizzaToppingDataset
 
 with open('node2id.json') as f:
     node2id = json.load(f)
 id2node = {id: node for node, id in node2id.items()}
 
+# TODO? Figure out how to save and load dgl models
+toppings_found = sys.argv[1:]
+# TODO abstract this out in find_toppings
+edges_fw = [
+    build_edge(top1, top2, node2id)
+    for top1, top2 in itertools.combinations(toppings_found, 2)
+]
+# Bidirectional edges becaues I don't know what I'm doing
+edges_bw = [
+    build_edge(top2, top1, node2id)
+    for top1, top2 in itertools.combinations(toppings_found, 2)
+]
+input_edges = edges_fw + edges_bw
+edge_df = pd.DataFrame.from_dict(input_edges)
+
+dataset = PizzaToppingDataset(edge_data=edge_df)
+g = dataset[0]
+
 with torch.no_grad():
     h = model(g, g.ndata['feat'])
     score = pred(g, h)
-    vals, indices = torch.topk(score, 50)
+    vals, indices = torch.topk(score, 10)
     print(indices)
     indices = indices[torch.hstack((indices.diff(), indices[-1])) > 1]
     print(indices)
