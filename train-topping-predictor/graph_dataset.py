2f2bbc285f0dd669040479a0c748f6f5c60491ff
Colin Brochtrup <cbrochtrup@conexiom.com>
2022-01-02 18:58:32 -0600

    Improve inference link prediction to make all nodes possible but use original graph information.

--- train-topping-predictor/graph_dataset.py
+++ train-topping-predictor/graph_dataset.py
@@ -11,9 +11,10 @@ class PizzaToppingDataset(DGLDataset):
         super().__init__(name='pizza_toppings')
 
     def process(self):
+        nodes_data = pd.read_csv('nodes.csv')
         edges_data = pd.read_csv('edges.csv')
+
         if self.edge_data_ is not None:
-          print('UUUUUUUUUUUUUUUUUUUU')
           edges_data = self.edge_data_
         edges_src = torch.from_numpy(edges_data['Src'].to_numpy())
         edges_dst = torch.from_numpy(edges_data['Dst'].to_numpy())
@@ -24,9 +25,7 @@ class PizzaToppingDataset(DGLDataset):
             )
             # gedges_data['Weight1'].to_numpy()
         ).float().T
-        print(edge_features.shape)
 
-        nodes_data = pd.read_csv('nodes.csv')
         # nodes_data = nodes_data[nodes_data.isin(edges_that_exist)]
 
         # node_labels = torch.from_numpy(nodes_data['Club'].astype('category').cat.codes.to_numpy())
--- train-topping-predictor/predict.py
+++ train-topping-predictor/predict.py
@@ -6,6 +6,7 @@ import sys
 from link_predict import model, pred
 from find_toppings import build_edge
 from graph_dataset import PizzaToppingDataset
+import dgl
 
 with open('node2id.json') as f:
     node2id = json.load(f)
@@ -13,31 +14,43 @@ id2node = {id: node for node, id in node2id.items()}
 
 # TODO? Figure out how to save and load dgl models
 toppings_found = sys.argv[1:]
-# TODO abstract this out in find_toppings
 edges_fw = [
     build_edge(top1, top2, node2id)
     for top1, top2 in itertools.combinations(toppings_found, 2)
 ]
-# Bidirectional edges becaues I don't know what I'm doing
 edges_bw = [
     build_edge(top2, top1, node2id)
     for top1, top2 in itertools.combinations(toppings_found, 2)
 ]
-input_edges = edges_fw + edges_bw
+# We want to consider all possible edges
+possible_edges = [
+    dict(
+        Src=node2id[top],
+        Dst=i,
+        Weight1=0,
+        Weight2=0
+    )
+    for top in toppings_found
+    for i in range(len(node2id))
+]
+input_edges = edges_fw + edges_bw + possible_edges
 edge_df = pd.DataFrame.from_dict(input_edges)
 
+d = PizzaToppingDataset()
+g_orig = d[0]
 dataset = PizzaToppingDataset(edge_data=edge_df)
 g = dataset[0]
 
+node_ids = [node2id[_] for _ in toppings_found]
+
 with torch.no_grad():
-    h = model(g, g.ndata['feat'])
+    h = model(g_orig, g_orig.ndata['feat'])
     score = pred(g, h)
-    vals, indices = torch.topk(score, 10)
-    print(indices)
-    indices = indices[torch.hstack((indices.diff(), indices[-1])) > 1]
-    print(indices)
-    all_preds = set()
-    for ind in indices:
-        n1, n2 = map(lambda x: x.item(), g.find_edges(ind))
-        all_preds.add((id2node[n1], id2node[n2]))
-    print(all_preds)
+    vals, indices = torch.topk(score, min(30, score.numel()))
+    valid_inds = [
+        g.edges()[1][ind.item()].item()
+        for ind in indices if ind not in node_ids
+    ]
+    # Don't select input toppings
+    toppings = set(id2node[_] for _ in valid_inds)
+    print(toppings)
